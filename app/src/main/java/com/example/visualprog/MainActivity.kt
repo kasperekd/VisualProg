6f225b3461a8ffa68c9a5df27b9e0aeff985a5db
kasperekd <kasperekd@mail.ru>
2024-09-22 19:15:35 +0700

    Request for access to geolocation. Displays current coordinates every second.

--- app/src/main/java/com/example/visualprog/MainActivity.kt
+++ app/src/main/java/com/example/visualprog/MainActivity.kt
@@ -4,15 +4,17 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.telephony.CellInfoCdma
+import android.telephony.CellInfoGsm
+import android.telephony.CellInfoLte
+import android.telephony.CellInfoWcdma
+import android.telephony.TelephonyManager
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,113 +22,115 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var latitudeTextView: TextView
     private lateinit var longitudeTextView: TextView
+    private lateinit var cellInfoTextView: TextView
+    private lateinit var telephonyManager: TelephonyManager
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
 
-    /**
-     * Called when the activity is starting.
-     * Initializes the layout, sets up the location client, and requests location permissions.
-     *
-     * @param savedInstanceState If the activity is being re-initialized after previously being shut down
-     * by the system, this Bundle contains the data it most recently supplied in
-     * [onSaveInstanceState]. Otherwise, it is null.
-     */
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Initialize TextView for latitude and longitude display
         latitudeTextView = findViewById(R.id.Latitude)
         longitudeTextView = findViewById(R.id.Longitude)
+        cellInfoTextView = findViewById(R.id.CellInfo)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // Initialize the FusedLocationProviderClient
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
 
-        // Initialize LocationRequest
         locationRequest = LocationRequest.create().apply {
-            interval = 1000 // 1 second
-            fastestInterval = 1000 // 1 second
+            interval = 1000
+            fastestInterval = 1000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
 
-        // Initialize LocationCallback
         locationCallback = object : LocationCallback() {
+            @SuppressLint("SetTextI18n")
             override fun onLocationResult(locationResult: LocationResult) {
                 for (location in locationResult.locations) {
-                    // Update the TextViews with the new location
                     latitudeTextView.text = "Latitude: ${location.latitude}"
                     longitudeTextView.text = "Longitude: ${location.longitude}"
+                    getCellInfo() // Получение информации о базовых станциях
                 }
             }
         }
 
-        // Request location permission
         requestLocationPermission()
     }
 
-    /**
-     * Requests permission to access the user's location.
-     * If permission is granted, it starts location updates.
-     */
     private fun requestLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Request permission
             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         } else {
-            // Permission already granted, start location updates
             startLocationUpdates()
         }
     }
 
-    /**
-     * Launcher for the permission request.
-     * Handles the result of the permission request.
-     *
-     * @param isGranted Indicates whether the permission was granted or denied.
-     */
     private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
         if (isGranted) {
-            // Permission granted, start location updates
             startLocationUpdates()
         } else {
             Toast.makeText(this, "Permission to access location denied", Toast.LENGTH_SHORT).show()
         }
     }
 
-    /**
-     * Starts location updates using the FusedLocationProviderClient.
-     * Updates the TextViews with the current location if permission is granted.
-     */
     private fun startLocationUpdates() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
         }
     }
 
-    /**
-     * Called when the activity is paused.
-     * Stops location updates to conserve battery and resources.
-     */
+    @SuppressLint("SetTextI18n")
+    private fun getCellInfo() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            val cellInfoList = telephonyManager.allCellInfo
+            if (cellInfoList != null) {
+                val cellInfoStringBuilder = StringBuilder()
+                for (cellInfo in cellInfoList) {
+                    when (cellInfo) {
+                        is CellInfoGsm -> {
+                            cellInfoStringBuilder.append("Type: GSM\n")
+                            cellInfoStringBuilder.append("Cell ID: ${cellInfo.cellIdentity.cid}\n")
+                            cellInfoStringBuilder.append("Signal Strength: ${cellInfo.cellSignalStrength.dbm} dBm\n")
+                            cellInfoStringBuilder.append("Location Area Code: ${cellInfo.cellIdentity.lac}\n")
+                        }
+                        is CellInfoLte -> {
+                            cellInfoStringBuilder.append("Type: LTE\n")
+                            cellInfoStringBuilder.append("Cell ID: ${cellInfo.cellIdentity.ci}\n")
+                            cellInfoStringBuilder.append("Signal Strength: ${cellInfo.cellSignalStrength.dbm} dBm\n")
+                            cellInfoStringBuilder.append("Tracking Area Code: ${cellInfo.cellIdentity.tac}\n")
+                        }
+                        is CellInfoCdma -> {
+                            cellInfoStringBuilder.append("Type: CDMA\n")
+                            cellInfoStringBuilder.append("Cell ID: ${cellInfo.cellIdentity.basestationId}\n")
+                            cellInfoStringBuilder.append("Signal Strength: ${cellInfo.cellSignalStrength.dbm} dBm\n")
+                        }
+                        is CellInfoWcdma -> {
+                            cellInfoStringBuilder.append("Type: WCDMA\n")
+                            cellInfoStringBuilder.append("Cell ID: ${cellInfo.cellIdentity.cid}\n")
+                            cellInfoStringBuilder.append("Signal Strength: ${cellInfo.cellSignalStrength.dbm} dBm\n")
+                            cellInfoStringBuilder.append("Location Area Code: ${cellInfo.cellIdentity.lac}\n")
+                        }
+                    }
+                    cellInfoStringBuilder.append("\n")
+                }
+                cellInfoTextView.text = "Cell Info:\n$cellInfoStringBuilder"
+            } else {
+                cellInfoTextView.text = "No cell info available"
+            }
+        } else {
+            requestLocationPermission()
+        }
+    }
+
     override fun onPause() {
         super.onPause()
-        // Stop location updates when the activity is not in the foreground
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
-    /**
-     * Called when the activity is resumed.
-     * Restarts location updates when the activity comes to the foreground.
-     */
     override fun onResume() {
         super.onResume()
-        // Restart location updates when the activity comes to the foreground
         startLocationUpdates()
     }
 }
